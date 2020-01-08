71231592b2fd890d58763bb0f2398cdd7c5dc936
leesangmyeong <leesangmyeong230@gmail.com>
2020-01-09 08:59:18 +0900

    Step02_DataType Package added

---
+++ Step02_DataType/src/test/main/MainClass06.java
@@ -0,0 +1,28 @@
+package test.main;
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		//변수 선언과 동시에 초기 값 대입 하기
+		int num=1;
+		String name="김구라";
+		//변수를 선언만 하기 
+		int num2;
+		String name2;
+		
+		//변수를 선언하고 초기값으로 기본값을 넣어주고 싶다면
+		int num3=0;
+		// null 은 참조데이터 type 이 담길수 있는 빈 공간을 만들때 사용한다. 
+		String name3=null;
+		
+		//빈공간에 참조값 대입하기 
+		name3="원숭이";
+		
+		System.out.println("main 메소드가 종료 됩니다.");
+	}
+}
+
+
+
+
+
